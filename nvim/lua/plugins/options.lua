377c5346a0fbe7f11c73d9758500080741fec97d
achntj <achntj@gmail.com>
2025-08-06 10:27:38 -0700

    feat: transparent bg nvim

--- nvim/lua/plugins/options.lua
+++ nvim/lua/plugins/options.lua
@@ -3,6 +3,16 @@
 --
 vim.cmd.colorscheme "mellow"
 
+-- Transaparent background
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+
 --
 -- NVIM TREE
 --
